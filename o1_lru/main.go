2cdff97200580fb550469dc89139f02a78cf3710
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-02-24 05:42:21 -0600

    Deque ready for LRU

--- o1_lru/main.go
+++ o1_lru/main.go
@@ -1,25 +1,48 @@
 package main
 
 type Node struct {
-  Key int
-  Value int
-  Next *Node
-  Prev *Node
+	Key   int
+	Value int
+	Next  *Node
+	Prev  *Node
 }
 
-type Deuque struct {
-  Head *Node
-  Tail *Node
+type Deque struct {
+	Head *Node
+	Tail *Node
 }
 
-func (d *Deque) PushFront(n *Node) {
-  if d.Head == nil {
-    d.Head = n
-    d.Tail = n
-  } else {
-    n.Next = d.Head
-    d.Head.Prev = n
-    d.Head = n
-  }
+func (d *Deque) PushFront(n Node) {
+	if d.Head == nil {
+		d.Head = &n
+		d.Tail = &n
+	} else {
+		n.Next = d.Head
+		d.Head.Prev = &n
+		d.Head = &n
+	}
+}
+
+func (d *Deque) PopBack() int {
+	if d.Tail != nil {
+		tail = d.Tail
+		d.Tail = d.Tail.Prev
+		d.Tail.Next = nil
+		return tail.Key
+	}
+	return -1
+}
+
+func (d *Deque) MoveToFront(n Node) {
+	if n.Prev != nil {
+		n.Prev.Next = n.Next
+	}
+	if n.Next != nil {
+		n.Next.Prev = n.Prev
+	}
+	d.PushFront(n)
+}
+
+type LRUCache struct {
 
 
