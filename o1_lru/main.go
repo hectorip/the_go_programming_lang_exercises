fb5969b7e3cecef5d7d8f4413bffc71419495178
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-02-25 23:58:31 -0600

    LRU with nodes first version

--- o1_lru/main.go
+++ o1_lru/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Key   int
 	Value int
@@ -25,7 +27,7 @@ func (d *Deque) PushFront(n Node) {
 
 func (d *Deque) PopBack() int {
 	if d.Tail != nil {
-		tail = d.Tail
+		tail := d.Tail
 		d.Tail = d.Tail.Prev
 		d.Tail.Next = nil
 		return tail.Key
@@ -44,5 +46,55 @@ func (d *Deque) MoveToFront(n Node) {
 }
 
 type LRUCache struct {
+	capacity    int
+	cache       map[int]Node
+	queue       Deque
+	currentSize int
+}
 
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		capacity:    capacity,
+		cache:       make(map[int]Node),
+		queue:       Deque{},
+		currentSize: 0,
+	}
+}
 
+func (lru *LRUCache) Get(key int) int {
+	if node, ok := lru.cache[key]; ok {
+		lru.queue.MoveToFront(node)
+		return node.Value
+	}
+	return -1
+}
+
+func (lru *LRUCache) Put(key int, value int) {
+	if node, ok := lru.cache[key]; ok {
+		node.Value = value
+		lru.queue.MoveToFront(node)
+		return
+	}
+	if lru.currentSize == lru.capacity {
+		delete(lru.cache, lru.queue.PopBack())
+		lru.currentSize--
+	}
+	new_node := Node{Key: key, Value: value}
+	lru.queue.PushFront(new_node)
+	lru.cache[key] = new_node
+	lru.currentSize++
+}
+
+func main() {
+	my_lru := Constructor(2)
+	my_lru.Put(1, 1)
+	my_lru.Put(2, 2)
+
+	fmt.Println(my_lru.Get(1))
+	my_lru.Put(3, 3)
+	fmt.Println(my_lru.Get(2))
+	my_lru.Put(4, 4)
+	fmt.Println(my_lru.Get(1))
+	fmt.Println(my_lru.Get(3))
+	fmt.Println(my_lru.Get(4))
+}
